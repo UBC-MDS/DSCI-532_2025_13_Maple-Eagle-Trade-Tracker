46493e9caa8e12f9e366fa4756c267962efc1fa8
Green-zy <zhou165@usc.edu>
2025-02-15 17:22:58 -0800

    draft UI of dashboard

--- src/app.py
+++ src/app.py
@@ -1,74 +1,43 @@
-from dash import Dash, html, dcc, Input, Output
-import dash_bootstrap_components as dbc
+from dash import Dash, html, dcc
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+app = Dash(__name__)
 
-app.layout = dbc.Container([
-    dbc.Row([
-        dbc.Col([  
-            dbc.Card([
-                dbc.CardBody([
-                    html.H4("Total Imports (CAD)", className="card-title"),
-                    html.H2("$XX Billion", id="kpi_imports", className="card-text")
-                ])
-            ], className="mb-4"),
-            dbc.Card([
-                dbc.CardBody([
-                    html.H4("Total Exports (CAD)", className="card-title"),
-                    html.H2("$XX Billion", id="kpi_exports", className="card-text")
-                ])
-            ])
-        ], width=3),
-        dbc.Col([  
-            dcc.Graph(id="trade_balance_chart")
-        ], width=9)
-    ], className="mb-4"),
-    
-    dbc.Row([
-        dbc.Col([  
-            dcc.Graph(id="import_bar_chart")
-        ], width=3),
-        dbc.Col([  
-            dcc.Graph(id="export_bar_chart")
-        ], width=3),
-        dbc.Col([  
-            dcc.Graph(id="trade_balance_map")
-        ], width=6)
-    ], className="mb-4"),
-    
-    dbc.Row([
-        dbc.Col([  
+app.layout = html.Div([
+    html.Div([
+        html.Div([
+            html.H4("Total Imports (CAD)"),
+            html.H2("$XX Billion", id="kpi_imports")
+        ], style={'border': '1px solid #ddd', 'padding': '10px', 'margin': '5px', 'display': 'inline-block'}),
+
+        html.Div([
+            html.H4("Total Exports (CAD)"),
+            html.H2("$XX Billion", id="kpi_exports")
+        ], style={'border': '1px solid #ddd', 'padding': '10px', 'margin': '5px', 'display': 'inline-block'})
+    ], style={'display': 'flex', 'justify-content': 'space-around'}),
+
+    dcc.Graph(id="trade_balance_chart"),
+
+    html.Div([
+        dcc.Graph(id="import_bar_chart", style={'width': '30%', 'display': 'inline-block'}),
+        dcc.Graph(id="export_bar_chart", style={'width': '30%', 'display': 'inline-block'}),
+        dcc.Graph(id="trade_balance_map", style={'width': '40%', 'display': 'inline-block'})
+    ], style={'display': 'flex', 'justify-content': 'space-around'}),
+
+    html.Div([
+        html.Div([
             html.Label("Select Trade Sector"),
-            dcc.Dropdown(id="sector_filter", options=[], multi=True)
-        ], width=3),
-        dbc.Col([  
+            dcc.Dropdown(id="sector_filter", options=[], multi=True),
+        ], style={'width': '20%', 'display': 'inline-block'}),
+
+        html.Div([
             html.Label("Select Province/Territory"),
-            dcc.Dropdown(id="province_filter", options=[], multi=True)
-        ], width=3),
-        dbc.Col([  
-            dcc.Graph(id="historical_import_chart")
-        ], width=3),
-        dbc.Col([  
-            dcc.Graph(id="historical_export_chart")
-        ], width=3)
-    ])
-], fluid=True)
+            dcc.Dropdown(id="province_filter", options=[], multi=True),
+        ], style={'width': '20%', 'display': 'inline-block'}),
 
-@app.callback(
-    Output("trade_balance_chart", "figure"),
-    Input("sector_filter", "value"),
-    Input("province_filter", "value")
-)
-def update_trade_balance_chart(sector, province):
-    return {}
-
-@app.callback(
-    Output("trade_balance_map", "figure"),
-    Input("sector_filter", "value"),
-    Input("province_filter", "value")
-)
-def update_trade_balance_map(sector, province):
-    return {}
+        dcc.Graph(id="historical_import_chart", style={'width': '30%', 'display': 'inline-block'}),
+        dcc.Graph(id="historical_export_chart", style={'width': '30%', 'display': 'inline-block'})
+    ], style={'display': 'flex', 'justify-content': 'space-around'})
+])
 
 if __name__ == '__main__':
     app.run(debug=True)
