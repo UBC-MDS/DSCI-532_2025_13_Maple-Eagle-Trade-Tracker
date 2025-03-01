b99ff8d1d41b3f4042d6117229531fc2ea745ed7
Alex Wong <likhang.wong97@gmail.com>
2025-02-28 20:54:17 -0800

    code for feature #4 and #5

--- src/app_alex.py
+++ src/app_alex.py
@@ -1,43 +1,89 @@
-from dash import Dash, html, dcc
+import altair as alt
+import pandas as pd
+import dash_bootstrap_components as dbc
+import dash_vega_components as dvc
+from dash import Dash, dcc, callback, Output, Input, html
+from vega_datasets import data
 
-app = Dash(__name__)
 
-app.layout = html.Div([
-    html.Div([
-        html.Div([
-            html.H4("Total Imports (CAD)"),
-            html.H2("$XX Billion", id="kpi_imports")
-        ], style={'border': '1px solid #ddd', 'padding': '10px', 'margin': '5px', 'display': 'inline-block'}),
+# Initiatlize the app
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-        html.Div([
-            html.H4("Total Exports (CAD)"),
-            html.H2("$XX Billion", id="kpi_exports")
-        ], style={'border': '1px solid #ddd', 'padding': '10px', 'margin': '5px', 'display': 'inline-block'})
-    ], style={'display': 'flex', 'justify-content': 'space-around'}),
+df = pd.read_csv('../data/clean/clean_data.csv')
 
-    dcc.Graph(id="trade_balance_chart"),
-
-    html.Div([
-        dcc.Graph(id="import_bar_chart", style={'width': '30%', 'display': 'inline-block'}),
-        dcc.Graph(id="export_bar_chart", style={'width': '30%', 'display': 'inline-block'}),
-        dcc.Graph(id="trade_balance_map", style={'width': '40%', 'display': 'inline-block'})
-    ], style={'display': 'flex', 'justify-content': 'space-around'}),
-
-    html.Div([
-        html.Div([
-            html.Label("Select Trade Sector"),
-            dcc.Dropdown(id="sector_filter", options=[], multi=True),
-        ], style={'width': '20%', 'display': 'inline-block'}),
-
-        html.Div([
-            html.Label("Select Province/Territory"),
-            dcc.Dropdown(id="province_filter", options=[], multi=True),
-        ], style={'width': '20%', 'display': 'inline-block'}),
-
-        dcc.Graph(id="historical_import_chart", style={'width': '30%', 'display': 'inline-block'}),
-        dcc.Graph(id="historical_export_chart", style={'width': '30%', 'display': 'inline-block'})
-    ], style={'display': 'flex', 'justify-content': 'space-around'})
+# Layout
+app.layout = dbc.Container([
+    dbc.Row([
+        dbc.Col([
+            html.H1('Maple-Eagle-Trade-Tracker'),
+            html.Br(),
+        ]),
+    ]),
+    dbc.Row([
+        dbc.Col(
+            [
+                dbc.Label('Select province'),
+                dcc.Checklist(id='province', 
+                 options=df['PROVINCE'].value_counts().index, 
+                 value=['Newfoundland and Labrador', 'Prince Edward Island', 'Nova Scotia',
+                        'New Brunswick', 'Quebec', 'Ontario', 'Manitoba', 'Saskatchewan',
+                        'Alberta', 'British Columbia', 'Yukon', 'Northwest Territories',
+                        'Nunavut']),
+                html.Br(),
+            ],
+            md=3
+        ),
+        dbc.Col(
+            [
+                dbc.Row(
+                    dbc.Col(
+                        dvc.Vega(id='bar1', spec={})     
+                    )
+                ),
+                dbc.Row(
+                    dbc.Col(
+                        dvc.Vega(id='bar2', spec={})     
+                    )
+                )
+            ],
+            md=4
+        )
+    ]),
 ])
 
+# Server side callbacks/reactivity
+@callback(
+    Output('bar1', 'spec'),
+    Input('province', 'value')
+)
+def create_chart(province):
+    return(
+        alt.Chart(df[(df['YEAR'] == 2024) & (df['TRADE_FLOW'] == 'Domestic export') & df['PROVINCE'].isin(province)]).mark_bar().encode(
+        x=alt.X('FULL_VALUE', title='Value'),
+        y=alt.Y('SECTOR', title='Sector', axis=alt.Axis(labelLimit=400, titlePadding=80)).sort('-x'),
+        ).properties(
+            width=500,
+            height=400,
+            title='Exports to the US in 2024 by sector'
+        ).to_dict()
+        )
+
+@callback(
+    Output('bar2', 'spec'),
+    Input('province', 'value')
+)
+def create_chart(province):
+    return(
+        alt.Chart(df[(df['YEAR'] == 2024) & (df['TRADE_FLOW'] == 'Import') & df['PROVINCE'].isin(province)]).mark_bar().encode(
+        x=alt.X('FULL_VALUE', title='Value'),
+        y=alt.Y('SECTOR', title='Sector', axis=alt.Axis(labelLimit=400, titlePadding=80)).sort('-x'),
+        ).properties(
+            width=500,
+            height=400,
+            title='Imports from the US in 2024 by sector'
+        ).to_dict()
+        )
+
+# Run the app/dashboard
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.server.run(port=8000, host='127.0.0.1')
