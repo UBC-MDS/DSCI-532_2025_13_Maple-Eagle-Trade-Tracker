a11c2f2f29e7173fcf3343103376ea0fdddb3c1c
Alex Wong <likhang.wong97@gmail.com>
2025-03-09 18:09:10 -0700

    pushing modularized codes up

---
+++ src/callbacks.py
@@ -0,0 +1,143 @@
+from dash import Dash, html, dcc, Input, Output, callback
+import dash_bootstrap_components as dbc
+import pandas as pd
+import altair as alt
+import dash_vega_components as dvc
+import geopandas as gpd
+from data import clean_data
+from components.inputs import (
+    province_options,
+    sector_options,
+    province_checklist,
+    sector_options,
+    sector_checklist)
+from components.outputs import(
+    create_net_trade_lineplot,
+    create_total_trade_card,
+    create_historical_chart,
+    get_agg_geom_data,
+    get_map_chart,
+    create_chart_card,
+    create_chart_card_trend_line,
+    create_control_card
+)
+
+df = clean_data()
+
+@callback(
+    [Output("import_card", "children"),
+     Output("export_card", "children")],
+    [Input("province-dropdown", "value"),
+     Input("sector-dropdown", "value")]
+)
+def update_total_trade_card(selected_provinces, selected_sectors):
+    filtered_df = df.copy()
+
+    if "All" not in selected_provinces:
+        filtered_df = filtered_df[filtered_df["PROVINCE"].isin(selected_provinces)]
+    if "All" not in selected_sectors:
+        filtered_df = filtered_df[filtered_df["SECTOR"].isin(selected_sectors)]
+
+    import_card_body = create_total_trade_card(filtered_df, "import").children  
+    export_card_body = create_total_trade_card(filtered_df, "export").children  
+
+    return import_card_body, export_card_body
+
+@callback(
+    Output("trade_balance_chart", "spec"),
+    [Input("province-dropdown", "value"),
+     Input("sector-dropdown", "value")]
+)
+def update_net_trade_lineplot(selected_provinces, selected_sectors):
+    filtered_df = df.copy()
+
+    if "All" not in selected_provinces:
+        filtered_df = filtered_df[filtered_df["PROVINCE"].isin(selected_provinces)]
+    if "All" not in selected_sectors:
+        filtered_df = filtered_df[filtered_df["SECTOR"].isin(selected_sectors)]
+
+    net_trade_lineplot = create_net_trade_lineplot(filtered_df)
+    return net_trade_lineplot
+
+@callback(
+    Output('bar1', 'spec'),
+    Input('province-dropdown', 'value')
+)
+def create_chart(province):
+    if "All" in province:
+        province = df['PROVINCE'].unique()
+    return(
+        alt.Chart(df[(df['YEAR'] == 2024) & (df['TRADE_FLOW'] == 'Domestic export') & df['PROVINCE'].isin(province)]).mark_bar().encode(
+        x=alt.X('sum(FULL_VALUE)', title='Value'),
+        y=alt.Y('SECTOR', title='Sector', axis=alt.Axis(labelLimit=400, titlePadding=80)).sort('-x'),
+        tooltip=[
+                alt.Tooltip('SECTOR', title='Sector:'),  
+                alt.Tooltip('sum(FULL_VALUE)', title='Total export value:', format=',')  
+            ]
+        ).properties(
+            width=260,
+            height=120,
+            title='Exports to the US in 2024 by sector'
+        ).to_dict()
+    )
+
+@callback(
+    Output('bar2', 'spec'),
+    Input('province-dropdown', 'value')
+)
+def create_chart(province):
+    if "All" in province:
+        province = df['PROVINCE'].unique()
+    return(
+        alt.Chart(df[(df['YEAR'] == 2024) & (df['TRADE_FLOW'] == 'Import') & df['PROVINCE'].isin(province)]).mark_bar().encode(
+        x=alt.X('sum(FULL_VALUE)', title='Value'),
+        y=alt.Y('SECTOR', title='Sector', axis=alt.Axis(labelLimit=400, titlePadding=80)).sort('-x'),
+        tooltip=[
+                alt.Tooltip('SECTOR', title='Sector:'),  
+                alt.Tooltip('sum(FULL_VALUE)', title='Total import value:', format=',') 
+            ]
+        ).properties(
+            width=260,
+            height=120,
+            title='Imports from the US in 2024 by sector'
+        ).to_dict()
+    )
+
+@callback(
+    Output("trade_geographical_map", "spec"),
+    [Input("province-dropdown", "value"),
+     Input("sector-dropdown", "value")]
+)
+def update_map_chart(selected_province, selected_sector):
+    """Updates the trade map based on user selections"""
+    
+    filtered_df = df.copy()
+
+    if selected_sector and (isinstance(selected_sector, list) and selected_sector != ['All']):
+        filtered_df = filtered_df[filtered_df["SECTOR"].isin(selected_sector)]
+
+    updated_chart = get_map_chart(filtered_df, selected_province)
+
+    return updated_chart.to_dict(format="vega")
+
+@callback(
+    [Output("historical_import_chart", "spec"),
+     Output("historical_export_chart", "spec")],
+    [Input("province-dropdown", "value"),
+     Input("sector-dropdown", "value")]
+)
+def update_historical_charts(selected_provinces, selected_sectors):
+    """Update the historical import and export charts based on dropdown selections."""
+
+    filtered_df = df.copy()
+
+    if "All" not in selected_provinces:
+        filtered_df = filtered_df[filtered_df["PROVINCE"].isin(selected_provinces)]
+
+    if "All" not in selected_sectors:
+        filtered_df = filtered_df[filtered_df["SECTOR"].isin(selected_sectors)]
+
+    import_chart = create_historical_chart(filtered_df[filtered_df["TRADE_FLOW"] == "Import"], "Annual Import")
+    export_chart = create_historical_chart(filtered_df[filtered_df["TRADE_FLOW"] == "Domestic export"], "Annual Export")
+
+    return import_chart, export_chart
---
+++ src/components/inputs.py
@@ -0,0 +1,45 @@
+from dash import dcc
+import pandas as pd
+from data import clean_data
+
+df = clean_data()
+
+province_options = [{'label': 'All', 'value': 'All'}] + [
+    {'label': province, 'value': province} for province in sorted(df['PROVINCE'].dropna().unique())
+]
+
+sector_options = [{'label': 'All', 'value': 'All'}] + [
+    {'label': sector, 'value': sector} for sector in sorted(df['SECTOR'].dropna().unique())
+]
+
+province_checklist = dcc.Checklist(
+    id='province-dropdown',
+    options=province_options,
+    value=[province_options[0]["value"]],  
+    inputStyle={"margin-right": "5px", "margin-left": "10px"},  
+    labelStyle={
+        "display": "block", 
+        "margin-left": "20px", 
+        "text-indent": "-29px",
+        "font-size": "12px"  
+    },
+    style={"max-width": "180px", "word-wrap": "break-word"}
+)
+
+sector_options = [
+    {'label': sector, 'value': sector} for sector in sorted(df['SECTOR'].dropna().unique())
+]
+
+sector_checklist = dcc.Checklist(
+    id='sector-dropdown',
+    options=sector_options,
+    value=[sector_options[1]["value"]],  
+    inputStyle={"margin-right": "5px", "margin-left": "10px"},  
+    labelStyle={
+        "display": "block", 
+        "margin-left": "20px", 
+        "text-indent": "-29px",
+        "font-size": "12px" 
+    },
+    style={"max-width": "180px", "word-wrap": "break-word"}
+)
